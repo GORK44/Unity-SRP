710a0f2aa066607b7d074420656f060a6a6eab7d
天线无双 <1069911023@qq.com>
2020-06-14 01:49:16 +0800

    Add files via upload

--- Assets/Custom RP/Shader/Unlit.shader
+++ Assets/Custom RP/Shader/Unlit.shader
@@ -1,14 +1,25 @@
 ﻿Shader "Custom RP/Unlit" {
     
     Properties {
+        _BaseMap("Texture", 2D) = "white" {}
         _BaseColor("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+        _Cutoff ("Alpha Cutoff", Range(0.0, 1.0)) = 0.5
+        [Toggle(_CLIPPING)] _Clipping ("Alpha Clipping", Float) = 0 //透明裁剪，默认关
+        [Enum(UnityEngine.Rendering.BlendMode)] _SrcBlend ("Src Blend", Float) = 1  //source 源 【枚举类型参数】
+        [Enum(UnityEngine.Rendering.BlendMode)] _DstBlend ("Dst Blend", Float) = 0  //destination 目标
+        [Enum(Off, 0, On, 1)] _ZWrite ("Z Write", Float) = 1
     }
     
     SubShader {
         
         Pass {
+            
+            Blend [_SrcBlend] [_DstBlend] //透明混合模式
+            ZWrite [_ZWrite]
+
             HLSLPROGRAM
-            #pragma multi_compile_instancing
+            #pragma shader_feature _CLIPPING  //裁剪
+            #pragma multi_compile_instancing  //实例化
             #pragma vertex UnlitPassVertex
             #pragma fragment UnlitPassFragment
             #include "UnlitPass.hlsl"
--- Assets/Custom RP/Shader/UnlitPass.hlsl
+++ Assets/Custom RP/Shader/UnlitPass.hlsl
@@ -13,20 +13,27 @@ cbuffer UnityPerMaterial {
 }
 */
 
+TEXTURE2D(_BaseMap);
+SAMPLER(sampler_BaseMap); //纹理采样
+
 //实例化
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST)
     UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+    UNITY_DEFINE_INSTANCED_PROP(float, _Cutoff)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 
 
 struct Attributes {     //用作vs的输入
     float3 positionOS : POSITION;
+    float2 baseUV : TEXCOORD0;  
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings {   //用作vs的输出，fs的输入
     float4 positionCS : SV_POSITION;
+    float2 baseUV : VAR_BASE_UV;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
@@ -40,6 +47,9 @@ Varyings UnlitPassVertex (Attributes input) {
     
     float3 positionWS = TransformObjectToWorld(input.positionOS.xyz); //模型空间->世界空间
     output.positionCS = TransformWorldToHClip(positionWS); //世界空间->裁剪空间
+
+    float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
+    output.baseUV = input.baseUV * baseST.xy + baseST.zw;
     
     return output;
     //return TransformObjectToHClip(input.positionOS.xyz); //模型空间->裁剪空间
@@ -53,7 +63,15 @@ Varyings UnlitPassVertex (Attributes input) {
 float4 UnlitPassFragment (Varyings input) : SV_TARGET {
 
     UNITY_SETUP_INSTANCE_ID(input);
-    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
+    float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    float4 base = baseMap * baseColor;
+
+    #if defined(_CLIPPING)  //透明裁剪
+        clip(base.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
+    #endif
+    
+    return base;
 
 }
 
